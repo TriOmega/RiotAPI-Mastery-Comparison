dc5b182a1f5037835fe096d3df04db40a480a88b
Eric O <ericdanielosorio@gmail.com>
2021-12-08 19:38:17 -0500

    Make application computer independent
    - Used WebClient in order to download the champion.json file rather than read it from a local path.

--- RiotAPI/UserControls/ChampionEntry.cs
+++ RiotAPI/UserControls/ChampionEntry.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.IO;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Web;
+using System.Net;
 using System.Collections.Generic;
 using System.Windows.Forms;
 using Newtonsoft.Json;
@@ -11,26 +8,17 @@ namespace RiotAPI.UserControls
 {
     public partial class ChampionEntry : UserControl
     {
-        private static readonly Uri championJSON = new Uri("http://ddragon.leagueoflegends.com/cdn/11.24.1/data/en_US/champion.json");
+        private static readonly Uri championURI = new Uri("http://ddragon.leagueoflegends.com/cdn/11.24.1/data/en_US/champion.json");
         private ChampionList ChampionNameList { get; set; }
-        public HttpClient Client { get; }
+        public WebClient Client { get; }
 
         public ChampionEntry(long masteryScore, long championID)
         {
             InitializeComponent();
 
-            Client = new HttpClient
-            {
-                MaxResponseContentBufferSize = 256000
-            };
-            Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-            //var query = Http
-
-           string championFileData = File.ReadAllText("D:\\Documents\\Current_Work\\RiotAPI\\RiotAPI-Mastery-Comparison\\RiotAPI\\champion.json");
-            //var query = 
-            
-            ChampionNameList = JsonConvert.DeserializeObject<ChampionList>(championFileData);
+            Client = new WebClient();
+            string championJSON = Client.DownloadString(championURI);
+            ChampionNameList = JsonConvert.DeserializeObject<ChampionList>(championJSON);
 
             foreach (KeyValuePair<string, Champion> entry in ChampionNameList.Data)
             {
